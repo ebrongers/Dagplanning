79420fef83db9d3d5d6c5557721bedaf29323fc5
Eric Brongers <ebrongers@proracom.nl>
2018-01-18 16:36:32 +0100

    Als functie

--- Dagplanning.ps1
+++ Dagplanning.ps1
@@ -2,21 +2,26 @@
 # XAML Code - Imported from Visual Studio Express WPF Application
 #==============================================================================================
 [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+function LoadForm($formname) {
+	$inputXML = Get-Content($formname)
+	$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N'  -replace '^<Win.*', '<Window'
 
-$inputXML = Get-Content('MainWindow.xaml')
-$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N'  -replace '^<Win.*', '<Window'
+	[xml]$xaml = $inputXML
 
-[xml]$XAML = $inputXML
+	#Read XAML
+	$reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+	try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+	catch{Write-Host "Unable to load Windows.Markup.XamlReader. Some possible causes for this problem include: .NET Framework is missing PowerShell must be launched with PowerShell -sta, invalid XAML code was encountered."; exit}
 
-#Read XAML
-$reader=(New-Object System.Xml.XmlNodeReader $xaml) 
-try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
-catch{Write-Host "Unable to load Windows.Markup.XamlReader. Some possible causes for this problem include: .NET Framework is missing PowerShell must be launched with PowerShell -sta, invalid XAML code was encountered."; exit}
+	#===========================================================================
+	# Store Form Objects In PowerShell
+	#===========================================================================
+	$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name ($_.Name) -Value $Form.FindName($_.Name) -Scope Global}
+	return $Form
+}
 
-#===========================================================================
-# Store Form Objects In PowerShell
-#===========================================================================
-$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name ($_.Name) -Value $Form.FindName($_.Name)}
+$Form=LoadForm('MainWIndow.xaml')
+
 
 #===========================================================================
 # Functies 
@@ -31,10 +36,31 @@ Function btnVerwijderen-click{
 	$Form.Close()
 }
 
+Function filllbxAdressen {
+	
+	$arr_listAgencies | ForEach-Object {
+		$lbxAdressen.Items.Add($_) | Out-Null
+	}
+}
+
+Function createEmailUser($fullname,$email) {
+	if ($email -eq "") {
+	   $Label1.Text = "Fout: Geen adres aangemaakt! `n E-mail adres ongeldig"
+	   Start-Sleep -Seconds 2
+	   $Form.Close();
+		exit
+	}
+}
 
 #===========================================================================
 # Add events to Form Objects
 #===========================================================================
 $btnAnnuleren.Add_Click({btnAnnuleren-click})
 $btnVerwijderen.Add_Click({btnVerwijderen-click})
+
+#===========================================================================
+# Main programma
+#===========================================================================
+$arr_listAgencies="ADMIN1","ADMIN2"
+filllbxAdressen
 $Form.ShowDialog() | out-null
